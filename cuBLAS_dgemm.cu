d1fb55aee83a70a8a6177811e6b53cc507ee238a
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2025-08-16 19:00:19 -0600

    Fix names from sgemm to dgemm

--- cuBLAS_dgemm.cu
+++ cuBLAS_dgemm.cu
@@ -17,44 +17,44 @@ int main(int argc, char *argv[]) {
 
   int i, j;
 
-  float *a, *b, *c;
+  double *a, *b, *c;
 
   // malloc for a,b,c...
-  a = (float *)malloc(m * k * sizeof(float));
-  b = (float *)malloc(k * n * sizeof(float));
-  c = (float *)malloc(m * n * sizeof(float));
+  a = (double *)malloc(m * k * sizeof(double));
+  b = (double *)malloc(k * n * sizeof(double));
+  c = (double *)malloc(m * n * sizeof(double));
 
   int ind = 11;
   for (j = 0; j < m * k; j++) {
-    a[j] = (float)ind++;
+    a[j] = (double)ind++;
   }
 
   ind = 11;
   for (j = 0; j < k * n; j++) {
-    b[j] = (float)ind++;
+    b[j] = (double)ind++;
   }
 
   ind = 11;
   for (j = 0; j < m * n; j++) {
-    c[j] = (float)ind++;
+    c[j] = (double)ind++;
   }
 
   // DEVICE
-  float *d_a, *d_b, *d_c;
+  double *d_a, *d_b, *d_c;
 
   // cudaMalloc for d_a, d_b, d_c...
-  cudaMalloc((void **)&d_a, m * k * sizeof(float));
-  cudaMalloc((void **)&d_b, k * n * sizeof(float));
-  cudaMalloc((void **)&d_c, m * n * sizeof(float));
+  cudaMalloc((void **)&d_a, m * k * sizeof(double));
+  cudaMalloc((void **)&d_b, k * n * sizeof(double));
+  cudaMalloc((void **)&d_c, m * n * sizeof(double));
 
   stat = cublasCreate(&handle); // initialize CUBLAS context
 
-  cudaMemcpy(d_a, a, m * k * sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, b, k * n * sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_c, c, m * n * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_a, a, m * k * sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, b, k * n * sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_c, c, m * n * sizeof(double), cudaMemcpyHostToDevice);
 
-  float alpha = 1.0f;
-  float beta = 0.5f;
+  double alpha = 1.0f;
+  double beta = 0.5f;
 
   if (print == 1) {
     printf("alpha = %4.0f, beta = %4.0f\n", alpha, beta);
@@ -81,10 +81,10 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  stat = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, d_b, n,
+  stat = cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, d_b, n,
                      d_a, k, &beta, d_c, n);
 
-  cudaMemcpy(c, d_c, m * n * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(c, d_c, m * n * sizeof(double), cudaMemcpyDeviceToHost);
 
   if (print == 1) {
     printf("\nC after SGEMM = \n");
